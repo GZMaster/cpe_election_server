f0d3a0de66530cde894993d8c6dea2c95f676f2d
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-02-01 03:57:09 +0100

    removing signup admin

--- routes/accreditationRoutes.js
+++ routes/accreditationRoutes.js
@@ -5,7 +5,7 @@ const adminController = require("../controllers/adminController");
 const router = express.Router();
 
 router.post("/login", adminController.login);
-router.post("/signup", adminController.signup);
+// router.post("/signup", adminController.signup);
 
 router
   .route("/")
